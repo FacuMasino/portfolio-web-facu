5d203d4a409554c55debd862a99603a5954da38f
facumasino <facujmasino@gmail.com>
2024-02-17 18:23:43 -0300

    SEO Optimizations

--- app/[lang]/components/AboutSection.tsx
+++ app/[lang]/components/AboutSection.tsx
@@ -1,10 +1,8 @@
 'use client';
 import React, { useState, useTransition } from 'react';
-import { Locale } from '@/i18n.config';
-import { getDictionary } from '@/lib/dictionary';
 import Image from 'next/image';
 import TabButton from './TabButton';
-import { imageKitLoader } from '@/app/utils/helpers';
+import useWindowSize, { imageKitLoader } from '@/app/utils/helpers';
 
 type AboutSections = {
   title: string;
@@ -20,7 +18,8 @@ type AboutLang = {
 const AboutSection = ({ about }: { about: AboutLang }) => {
   const [tab, setTab] = useState(0);
   const [isPending, startTransition] = useTransition();
-
+  const { width } = useWindowSize();
+  const isMobile = width && width <= 768;
   const handleTabChange = (id: number) => {
     startTransition(() => {
       setTab(id);
@@ -29,16 +28,16 @@ const AboutSection = ({ about }: { about: AboutLang }) => {
   return (
     <section id="about">
       <div className="items-center gap-8 px-4 py-8 text-white sm:py-16 md:grid md:grid-cols-2 xl:gap-16">
-        <Image
+        {isMobile ? "" : <Image
           loader={imageKitLoader}
-          className="rounded"
+          className="rounded hidden md:block"
           src="aboutimg.jpg?updatedAt=1708120599646"
           alt="hero image"
           width={500}
           height={500}
           quality={50}
           loading={'eager'}
-        />
+        />}
         <div className="mt-8 flex h-full flex-col text-left md:mt-0">
           <h2 className="pb-4 text-2xl font-bold md:text-3xl">{about.title}</h2>
           <p className="text-base lg:text-lg">{about.description}</p>
--- app/utils/helpers.tsx
+++ app/utils/helpers.tsx
@@ -1,4 +1,5 @@
 import { ImageLoaderProps } from "next/image";
+import { useEffect, useState } from 'react';
 
 export const sendEmail = async (data: string): Promise<boolean> => {
   const endpoint = '/api/send';
@@ -42,3 +43,30 @@ export const imageKitLoader = ({ src, width, quality }: ImageLoaderProps) => {
   if(urlEndpoint[urlEndpoint.length-1] === "/") urlEndpoint = urlEndpoint.substring(0, urlEndpoint.length - 1);
   return `${urlEndpoint}/${src}?tr=${paramsString}`
 }
+
+type WindowSize = {
+    width: number | undefined;
+    height: number | undefined;
+};
+
+const useWindowSize = (): WindowSize => {
+    const [WindowSize, setWindowSize] = useState<WindowSize>({
+        width: undefined,
+        height: undefined,
+    });
+    useEffect(() => {
+        function handleResize(): void {
+            setWindowSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        }
+        handleResize();
+        window.addEventListener('resize', handleResize);
+        return (): void => window.removeEventListener('resize', handleResize);
+    }, []); // Empty array ensures that effect is only run on mount
+
+    return WindowSize;
+};
+
+export default useWindowSize;
