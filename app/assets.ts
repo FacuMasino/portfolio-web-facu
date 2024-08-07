3879eaa74283a0672798f5f1b4d10646baf29c8a
Facundo Masino <facujmasino@gmail.com>
2024-08-07 20:07:09 -0300

    Add new project

--- app/assets.ts
+++ app/assets.ts
@@ -1,10 +1,3 @@
-const img1 = '/images/projects/1.png';
-const img2 = '/images/projects/2.png';
-const img3 = '/images/projects/3.png';
-const img4 = '/images/projects/4.png';
-const img5 = '/images/projects/5.png';
-const img6 = '/images/projects/6.png';
-
 export type TagsType = 'all' | 'web' | 'desktop';
 export type ProjectsDataType = {
   id: number;
@@ -17,46 +10,53 @@ export type ProjectsDataType = {
 export const ProjectsData: ProjectsDataType = [
   {
     id: 1,
-    image: img1,
+    image: '/images/projects/1.png',
     tags: ['all', 'web'],
     link: 'https://pedime.netlify.app/',
     github: 'https://github.com/FacuMasino/pedime-reactjs-masino',
   },
   {
     id: 2,
-    image: img2,
+    image: '/images/projects/2.png',
     tags: ['all', 'web'],
     link: 'https://facumasino.github.io/CotizadorSeguro-JS-Masino/',
     github: 'https://github.com/FacuMasino/CotizadorSeguro-JS-Masino',
   },
   {
     id: 3,
-    image: img3,
+    image: '/images/projects/3.png',
     tags: ['all', 'web'],
     link: 'https://bi-consulting-web-testing.vercel.app/',
     github: 'https://github.com/FacuMasino/bi-consulting-web',
   },
   {
     id: 4,
-    image: img4,
+    image: '/images/projects/4.png',
     tags: ['all', 'desktop'],
     link: 'https://github.com/FacuMasino/tp-integrador-grupo-10-lab2',
     github: 'https://github.com/FacuMasino/tp-integrador-grupo-10-lab2',
   },
   {
     id: 5,
-    image: img5,
+    image: '/images/projects/5.png',
     tags: ['all', 'desktop'],
     link: 'https://github.com/FacuMasino/imprimir-polizas-utility',
     github: 'https://github.com/FacuMasino/imprimir-polizas-utility',
   },
   {
     id: 6,
-    image: img6,
+    image: '/images/projects/6.png',
     tags: ['all', 'web'],
     link: 'https://comparatasas.ar',
     github:
       'https://github.com/ferminrp/compara-tasas/commits?author=FacuMasino',
+  },
+  {
+    id: 7,
+    image: '/images/projects/7.png',
+    tags: ['all', 'web'],
+    link: 'https://github.com/FacuMasino/ecommerce-net',
+    github: 'https://github.com/FacuMasino/ecommerce-net',
   },
 ];
 
